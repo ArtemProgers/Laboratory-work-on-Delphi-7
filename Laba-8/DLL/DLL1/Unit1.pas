1059b6baa1f9b9664f609c39be13c00b28f9bcd9
Artem <128305840+ArtemProgers@users.noreply.github.com>
2024-01-03 23:57:32 +0300

    Add files via upload

---
+++ Laba-8/DLL/DLL1/Unit1.pas
@@ -0,0 +1,47 @@
+unit Unit1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Edit1: TEdit;
+    Label1: TLabel;
+    Button1: TButton;
+    Label2: TLabel;
+    Label3: TLabel;
+    procedure Button1Click(Sender: TObject);
+  private
+
+  public
+    
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+function SpecFunc(x:Single):Single; external 'Calc.dll';
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+  x:Single;
+  z1:Single;
+begin
+  x:=StrToFloat(Edit1.Text);
+  try
+    z1:=SpecFunc(x);
+    Label3.Caption:=FloatToStr(z1);
+  except
+    On E: Exception do ShowMessage(E.Message)
+    else
+      ShowMessage('Неизвестная ошибка');
+  end;
+end;
+
+end.
