16ffb99b87da3fee959fdd9b2958a3a6d29b1b44
Artem <128305840+ArtemProgers@users.noreply.github.com>
2024-01-03 23:52:18 +0300

    Add files via upload

---
+++ Laba-6/Unit1.pas
@@ -0,0 +1,73 @@
+unit Unit1;
+
+interface
+
+uses
+    Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, ComCtrls, StdCtrls, ExtCtrls, TLHelp32, Grids;
+
+type
+  TForm1 = class(TForm)
+    StringGrid1: TStringGrid;
+    Button1: TButton;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+  hSnapshot: THandle;
+  pe32: TProcessEntry32;
+  rowIndex: Integer;
+begin
+
+  StringGrid1.DefaultColWidth := 150;
+  StringGrid1.DefaultRowHeight := 25;
+
+  StringGrid1.Cells[0,0]:='Имя файла';
+  StringGrid1.Cells[1,0]:='Процесс ID';
+  //StringGrid1.Cells[2,0]:='Родительский процесс ID';
+  StringGrid1.Cells[2,0]:='Родительский наименование'
+  StringGrid1.RowCount := 1;
+  rowIndex := 1;
+
+  hSnapshot := CreateToolHelp32Snapshot(TH32CS_SNAPPROCESS, 0); //снимок списка всех процессов
+  if hSnapshot <> INVALID_HANDLE_VALUE then
+  begin
+    pe32.dwSize := SizeOf(TProcessEntry32);  //размер структуры
+    if Process32First(hSnapshot, pe32) then  // инфа о первом процессе
+    begin
+      repeat
+        StringGrid1.Cells[0, rowIndex] := pe32.szExeFile;
+        StringGrid1.Cells[1, rowIndex] := IntToStr(pe32.th32ProcessID);
+        //StringGrid1.Cells[2, rowIndex] := IntToStr(pe32.th32ParentProcessID);
+        StringGrid1.Cells[2, rowIndex] := IntToStr(pe32.GetProcessFileName);
+        StringGrid1.RowCount := StringGrid1.RowCount + 1;
+        Inc(rowIndex);
+
+      until not Process32Next(hSnapshot, pe32); //идём к следующему процессу
+    end
+    else
+    begin
+      ShowMessage('Error when getting information about the process: ' + SysErrorMessage(GetLastError));
+    end;
+
+    CloseHandle(hSnapshot);   //закрываем дескриптор
+  end
+  else
+  begin
+    ShowMessage('Error when creating a snapshot of the process: ' + SysErrorMessage(GetLastError));
+  end;
+end;
+
+end.
---
+++ Laba-6/Unit1.~dfm
@@ -0,0 +1,34 @@
+object Form1: TForm1
+  Left = 570
+  Top = 207
+  BorderStyle = bsSingle
+  Caption = #1057#1087#1080#1089#1086#1082' '#1074#1089#1077#1093' '#1087#1088#1086#1094#1077#1089#1089#1086#1074' '#1080' '#1085#1072#1080#1084#1077#1085#1086#1074#1072#1085#1080#1103'  '#1088#1086#1076#1080#1090#1077#1083#1100#1089#1082#1080#1093' '#1087#1088#1086#1094#1077#1089#1089#1086#1074' '
+  ClientHeight = 414
+  ClientWidth = 683
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'MS Sans Serif'
+  Font.Style = []
+  FormStyle = fsStayOnTop
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object StringGrid1: TStringGrid
+    Left = 16
+    Top = 16
+    Width = 657
+    Height = 353
+    TabOrder = 0
+  end
+  object Button1: TButton
+    Left = 24
+    Top = 376
+    Width = 75
+    Height = 25
+    Caption = #1054#1073#1085#1086#1074#1080#1090#1100
+    TabOrder = 1
+    OnClick = Button1Click
+  end
+end
